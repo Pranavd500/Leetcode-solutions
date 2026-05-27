9e35c00ba501b6898569c1a72253c6de370a7a7e
Pranavd500 <114133861+Pranavd500@users.noreply.github.com>
2026-05-27 22:03:18 +0530

    Time: 0 ms (100%), Space: 20.3 MB (66.61%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,11 +1,14 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashmap ={}
+        # Hash map to store: {value: index}
+        prevMap = {} 
         
-        for i,num in enumerate(nums):
-            diff=target-num
-
-            if diff in hashmap:
-                return [hashmap[diff],i]
-            hashmap[num]=i
+        for i, n in enumerate(nums):
+            diff = target - n
+            # If the complement exists in the map, we found the pair
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            # Otherwise, add current number and its index to the map
+            prevMap[n] = i
         
+        return [] 
